880a32a694e4de30d6b3c4517047c625d0eaf810
Simon <Crotexrl@gmail.com>
2019-01-20 22:35:11 +0100

    Update ScheduleEdit.java

--- app/src/main/java/com/stauss/simon/stundenplan/ScheduleEdit.java
+++ app/src/main/java/com/stauss/simon/stundenplan/ScheduleEdit.java
@@ -54,23 +54,11 @@ public class ScheduleEdit extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_schedule_edit, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -128,8 +116,8 @@ public class ScheduleEdit extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 3;
+            // Show 5 total pages. Mo-Fr
+            return 5;
         }
     }
 }

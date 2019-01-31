775e02f567a53c4e5cb4176df191cd432659268a
Simon <Crotexrl@gmail.com>
2019-01-31 16:54:28 +0100

    Fragment Code Cleanup

--- app/src/main/java/com/stauss/simon/stundenplan/ScheduleOverviewFragment.java
+++ app/src/main/java/com/stauss/simon/stundenplan/ScheduleOverviewFragment.java
@@ -13,86 +13,47 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ScheduleOverviewFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ScheduleOverviewFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ScheduleOverviewFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
     SharedPreferences sharedPreferences;
-    SharedPreferences.Editor prefEdit;
 
     String[] days;
 
-    MainSchedule main;
-
     public ScheduleOverviewFragment() {
-        // Required empty public constructor
+        //"Required empty public constructor" - Android
     }
 
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment ScheduleOverviewFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ScheduleOverviewFragment newInstance(String param1, String param2) {
+    public static ScheduleOverviewFragment newInstance() {
         ScheduleOverviewFragment fragment = new ScheduleOverviewFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
 
         days = getMain().getWeek();
 
         sharedPreferences = getActivity().getSharedPreferences(getString(R.string.preference_key), Context.MODE_PRIVATE);
-        prefEdit = sharedPreferences.edit();
-
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_schedule_overview, container, false);
 
-        buildTable((TableLayout) v.findViewById(R.id.table));
+        buildSchedule((TableLayout) v.findViewById(R.id.table));
+
         // Inflate the layout for this fragment
         return v;
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
     }
 
     @Override
@@ -123,17 +84,14 @@ public class ScheduleOverviewFragment extends Fragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 
-
     private MainSchedule getMain() {
-        main = new MainSchedule();
-        return main;
+        return new MainSchedule();
     }
 
-    private void buildTable(TableLayout table) {
+    private void buildSchedule(TableLayout table) {
         TableRow row;
         int c;
         for (int i = 1; i <= 11; i++) {
--- app/src/main/java/com/stauss/simon/stundenplan/ScheduleTodayFragment.java
+++ app/src/main/java/com/stauss/simon/stundenplan/ScheduleTodayFragment.java
@@ -16,92 +16,53 @@ import android.widget.TextView;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ScheduleTodayFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ScheduleTodayFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ScheduleTodayFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
     SharedPreferences sharedPreferences;
-    SharedPreferences.Editor prefEdit;
 
     String day;
 
     public ScheduleTodayFragment() {
-        // Required empty public constructor
+        //"Required empty public constructor" - Android
     }
 
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment ScheduleTodayFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static ScheduleTodayFragment newInstance(String param1, String param2) {
+    public static ScheduleTodayFragment newInstance() {
         ScheduleTodayFragment fragment = new ScheduleTodayFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
 
         day = getMain().getDay();
+        
         sharedPreferences = getActivity().getSharedPreferences(getString(R.string.preference_key), Context.MODE_PRIVATE);
-        prefEdit = sharedPreferences.edit();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_schedule_today, container, false);
-
-        TableLayout table = v.findViewById(R.id.table);
 
         TextView text = v.findViewById(R.id.textView);
         text.setVisibility(View.VISIBLE);
         String date = new SimpleDateFormat("EEEE, dd. MMMM yyyy").format(new Date());
         text.setText("Heute, " + date + ", hast du folgende Fächer:" );
 
-        buildDailySchedule((TableLayout) v.findViewById(R.id.table));
+        buildSchedule((TableLayout) v.findViewById(R.id.table));
 
         // Inflate the layout for this fragment
         return v;
     }
 
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
     @Override
     public void onAttach(Context context) {
@@ -131,11 +92,14 @@ public class ScheduleTodayFragment extends Fragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 
-    private void buildDailySchedule(TableLayout tableLayout) {
+    private MainSchedule getMain() {
+        return new MainSchedule();
+    }
+
+    private void buildSchedule(TableLayout tableLayout) {
         for (int i = 1; i <= 11; i++) {
             TableRow row = new TableRow(getContext());
             row.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
@@ -160,9 +124,5 @@ public class ScheduleTodayFragment extends Fragment {
 
             tableLayout.addView(row);
         }
-    }
-
-    private MainSchedule getMain() {
-        return new MainSchedule();
     }
 }
